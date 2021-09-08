c39b4e2d01b1f860e1a9762a35dc111ed629530a
Wen Xiao <wennstyle@gmail.com>
2021-09-07 23:54:06 -0400

    commit tree d88f965d485a4a0760b366d8fcfce3c3251c0af9

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`; //code is more efficient than the above
+//uses backtick to interpret the expression within the as interpolate variable rather than an actual string (when using quotes)
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
